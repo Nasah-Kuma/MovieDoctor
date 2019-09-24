e9582e5b87aa6a38cb0b09d0fed0eef375b709a6
NasahK <nasahnash19@gmail.com>
2019-09-24 11:24:23 +0100

    Just testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { MovieItemComponent } from './movie-item/movie-item.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieItemComponent } from './movies/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
--- src/app/movies/movie.model.ts
+++ src/app/movies/movie.model.ts
@@ -2,8 +2,9 @@ export class Movie{
     public name: string;
     public description: string;
     public imagePath: string ;
+    public releaseDate: Date;
 
-    constructor(name:string, desc:string, imagePath:string){
+    constructor(name:string, desc:string, imagePath:string, releaseDate:Date){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
